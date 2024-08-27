601a8276bc1634750607237eea333baf700c299a
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-08-27 16:29:00 -0300

    Initial commit

---
+++ backend/src/main/java/br/com/akrasia/akimob/AkimobApplication.java
@@ -0,0 +1,13 @@
+package br.com.akrasia.akimob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkimobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkimobApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/akrasia/akimob/AkimobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.akrasia.akimob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkimobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
