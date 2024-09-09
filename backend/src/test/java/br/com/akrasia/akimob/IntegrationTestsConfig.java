00c0f1cae51850de64c199c49899695c67d307dc
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-09-09 16:35:20 -0300

    build(back): use flyway migrations

--- backend/src/test/java/br/com/akrasia/akimob/IntegrationTestsConfig.java
+++ backend/src/test/java/br/com/akrasia/akimob/IntegrationTestsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.MountableFile;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class IntegrationTestsConfig {
@@ -12,12 +11,7 @@ public class IntegrationTestsConfig {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgreSQLContainer() {
-        return new PostgreSQLContainer<>("postgres:16-alpine")
-                .withCopyFileToContainer(
-                        // Use migration scripts from src/main/resources
-                        MountableFile.forHostPath("src/main/resources/db/migration"),
-                        "/docker-entrypoint-initdb.d/");
-
+        return new PostgreSQLContainer<>("postgres:16-alpine");
     }
 
 }
