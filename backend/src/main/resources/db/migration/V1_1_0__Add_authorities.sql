d38cc0b5c5dfa5df834bd457c5088041a6341a3f
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-09-11 16:35:33 -0300

    feat(db): add role group and address authorities

---
+++ backend/src/main/resources/db/migration/V1_1_0__Add_authorities.sql
@@ -0,0 +1,9 @@
+INSERT INTO authority (name, description) VALUES
+    ('role_group_add', 'Create new role groups'),
+    ('role_group_read', 'Read role groups'),
+    ('role_group_edit', 'Edit role groups'),
+    ('role_group_delete', 'Delete role groups'),
+    ('address_add', 'Create new addresses'),
+    ('address_read', 'Read addresses'),
+    ('address_edit', 'Edit addresses'),
+    ('address_delete', 'Delete addresses');
