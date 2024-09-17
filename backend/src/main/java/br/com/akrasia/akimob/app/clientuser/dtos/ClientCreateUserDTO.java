c472bf8c951cc079ec33d72a51014f48f7a6febe
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-09-17 17:33:07 -0300

    fix(back): add @NotNull validation on createClientUser

--- backend/src/main/java/br/com/akrasia/akimob/app/clientuser/dtos/ClientCreateUserDTO.java
+++ backend/src/main/java/br/com/akrasia/akimob/app/clientuser/dtos/ClientCreateUserDTO.java
@@ -1,7 +1,8 @@
 package br.com.akrasia.akimob.app.clientuser.dtos;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
-public record ClientCreateUserDTO(@Min(1) Long userId, @Min(1) Long roleGroupId) {
+public record ClientCreateUserDTO(@NotNull @Min(1) Long userId, @NotNull @Min(1) Long roleGroupId) {
 
 }
