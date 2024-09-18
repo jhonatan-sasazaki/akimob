b555538afe9323023f9a9f3b92aa93d3afd06b62
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-09-18 16:39:53 -0300

    feat(back): handle MethodArgumentNotValidExceptions

--- backend/src/main/java/br/com/akrasia/akimob/core/exception/CustomExceptionHandler.java
+++ backend/src/main/java/br/com/akrasia/akimob/core/exception/CustomExceptionHandler.java
@@ -1,10 +1,13 @@
 package br.com.akrasia.akimob.core.exception;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,11 +16,26 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<ProblemDetail> handleSQLException(SQLException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);        
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
         problemDetail.setTitle("SQL Exception");
         problemDetail.setDetail(ex.getMessage());
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("Method Argument Not Valid Exception");
+
+        Map<String, String> fieldErrors = new HashMap<>();
+        ex.getFieldErrors().forEach(fieldError -> {
+            fieldErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+
+        problemDetail.setProperty("fields", fieldErrors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
     }
 
 }
