27fc214c93f9e2b6bbc8176d8550690e412d0491
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-09-19 13:16:34 -0300

    tests(back): remove clearDatabase from IntegrationTests base class

--- backend/src/test/java/br/com/akrasia/akimob/core/superadmin/SuperadminClientIntegrationTests.java
+++ backend/src/test/java/br/com/akrasia/akimob/core/superadmin/SuperadminClientIntegrationTests.java
@@ -3,14 +3,20 @@ package br.com.akrasia.akimob.core.superadmin;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.akrasia.akimob.IntegrationTests;
@@ -21,10 +27,16 @@ import br.com.akrasia.akimob.core.client.ClientRepository;
 import br.com.akrasia.akimob.core.client.ClientService;
 import br.com.akrasia.akimob.core.user.UserService;
 import br.com.akrasia.akimob.core.user.dtos.UserCreateDTO;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@Slf4j
 public class SuperadminClientIntegrationTests extends IntegrationTests {
+
+    @Autowired
+    @Qualifier("client")
+    private DataSource clientDataSource;
 
     @Autowired
     private WebTestClient webTestClient;
@@ -47,9 +59,18 @@ public class SuperadminClientIntegrationTests extends IntegrationTests {
     }
 
     @BeforeEach
-    void clearClient() {
+    public void clearClient() throws SQLException {
+        log.info("Clearing client table");
+
         clientRepository.deleteAll();
         clientRepository.flush();
+
+        log.info("Clearing client database");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(clientDataSource);
+        String sql = "SELECT schema_name FROM information_schema.schemata WHERE (schema_name NOT LIKE 'pg_%') and (schema_name != 'information_schema');";
+        jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("schema_name"))
+                .forEach(schemaName -> jdbcTemplate.execute("DROP SCHEMA IF EXISTS " + schemaName + " CASCADE"));
     }
 
     @Test
