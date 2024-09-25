1ef19d7f3519550aeeffda1355ece20a45ccb381
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-09-25 17:19:52 -0300

    feat(front): default redirect to dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './core/auth/login/login.component';
 import { MainComponent } from './core/main/main.component';
 import { authGuard } from './core/auth/authentication.guard';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: MainComponent, canMatch: [authGuard] },
+  {
+    path: '',
+    component: MainComponent,
+    canMatch: [authGuard],
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      { path: 'dashboard', component: DashboardComponent },
+    ],
+  },
+  { path: '**', redirectTo: '' },
 ];
