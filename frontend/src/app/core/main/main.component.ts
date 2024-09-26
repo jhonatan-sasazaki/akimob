0ecfdb9c6bd84e35fecf29c18668764e0cafa377
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-09-26 10:19:37 -0300

    feat(front): create toolbar and side navbar

--- frontend/src/app/core/main/main.component.ts
+++ frontend/src/app/core/main/main.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbar,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+  title = environment.appName;
+}
