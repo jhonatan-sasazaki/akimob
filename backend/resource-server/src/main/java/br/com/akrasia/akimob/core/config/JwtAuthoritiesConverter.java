1192c353386b6e902fb2e2a828e9a6d45f8e2620
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-10-08 17:16:11 -0300

    feat(back): read authorities from jwt claims

---
+++ backend/resource-server/src/main/java/br/com/akrasia/akimob/core/config/JwtAuthoritiesConverter.java
@@ -0,0 +1,22 @@
+package br.com.akrasia.akimob.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+@Configuration(proxyBeanMethods = false)
+public class JwtAuthoritiesConverter {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+}
