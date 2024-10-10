09d8e770dd48d2b502fdc5e11cd042472156fe2d
Jhonatan Sasazaki <165798601+jhonatan-sasazaki@users.noreply.github.com>
2024-10-10 16:25:45 -0300

    fix(back): add authorities converter for the new JWT response claims

--- backend/resource-server/src/main/java/br/com/akrasia/akimob/core/config/JwtConfig.java
+++ backend/resource-server/src/main/java/br/com/akrasia/akimob/core/config/JwtConfig.java
@@ -3,19 +3,15 @@ package br.com.akrasia.akimob.core.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import br.com.akrasia.akimob.core.jwt.JwtGrantedAuthoritiesConverter;
 
 @Configuration(proxyBeanMethods = false)
-public class JwtAuthoritiesConverter {
+public class JwtConfig {
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
-        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
-
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new JwtGrantedAuthoritiesConverter());
         return jwtAuthenticationConverter;
     }
 
---
+++ backend/resource-server/src/main/java/br/com/akrasia/akimob/core/jwt/JwtGrantedAuthoritiesConverter.java
@@ -0,0 +1,46 @@
+package br.com.akrasia.akimob.core.jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import br.com.akrasia.akimob.commons.core.client.context.ClientContext;
+
+public class JwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (String authority : getAuthorities(jwt)) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority));
+        }
+        return grantedAuthorities;
+    }
+
+    private Collection<String> getAuthorities(Jwt jwt) {
+        Set<String> authorities = new HashSet<>();
+
+        Boolean superadmin = jwt.getClaimAsBoolean("superadmin");
+        if (superadmin) {
+            authorities.add("ROLE_SUPERADMIN");
+        }
+
+        Map<String, String[]> authoritiesClaim = jwt.getClaim("authorities");
+        String currentClient = ClientContext.getCurrentClient();
+        if (authoritiesClaim.containsKey(currentClient)) {
+            for (String authority : authoritiesClaim.get(currentClient)) {
+                authorities.add(authority);
+            }
+        }
+
+        return authorities;
+    }
+
+}
